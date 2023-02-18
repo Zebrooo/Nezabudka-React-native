1a73c59d0c8c0a890980f1387cd0a6698294f53e
LesyaBabets <lesya.romanovna23@gmail.com>
2023-02-19 02:43:35 +0300

    game with input date

--- client/src/screens/LoginScreen.tsx
+++ client/src/screens/LoginScreen.tsx
@@ -1,35 +1,63 @@
 import { Formik } from "formik";
 import React from "react";
-import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  Image,
+  Keyboard,
+  SafeAreaView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import styles from "../styles/stylesall";
+import logo from "../../assets/logo.png"
 
 export default function LoginScreen({ navigation }) {
   return (
-    <View style={styles.main}>
-      <Formik
-        initialValues={{ email: "", hashpass: "", username: "" }}
-        onSubmit={(values) => console.log(values)}
-      >
-        {(props) => (
-          <View style={{ marginTop: "40%", marginLeft: "17%" }}>
-            <Text style={styles.text}>Введите ваш логин</Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={props.handleChange("email")}
-              value={props.values.email}
-            ></TextInput>
-            <Text style={styles.text}>Введите ваш пароль</Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={props.handleChange("hashpass")}
-              value={props.values.hashpass}
-            ></TextInput>
-            <TouchableOpacity onPress={() => navigation.navigate('SignUpScreen')}>
-            <Text style={styles.textBtn}> Войти</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      </Formik>
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+        ("dismiss keyboard");
+      }}
+    >
+      <SafeAreaView style={styles.main}>
+      <Image
+          source={logo}
+          style={{
+            marginTop: 40, width: '100%', height: 160, position: 'absolute', top: 0, resizeMode: 'cover',
+          }}
+        />
+        <Formik
+          initialValues={{ email: "", hashpass: "", username: "" }}
+          onSubmit={(values) => console.log(values)}
+        >
+          {(props) => (
+            <View style={{ marginTop: "60%", marginLeft: "17%" }}>
+              <TextInput
+              placeholder="Введите вашу почту"
+                keyboardType="email-address"
+                style={styles.input}
+                onChangeText={props.handleChange("email")}
+                value={props.values.email}
+              ></TextInput>
+              <TextInput
+              placeholder="Введите ваш пароль"
+                style={styles.input}
+                onChangeText={props.handleChange("hashpass")}
+                value={props.values.hashpass}
+              ></TextInput>
+              <TouchableOpacity
+              style={styles.botton}
+                onPress={() => navigation.navigate("SignUpScreen")}
+              >
+                <Text style={styles.textBtn}> Войти</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </Formik>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
