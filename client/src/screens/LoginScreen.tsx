bb101cfb095b3419d3324ab8cd0ef29ebbd1c6bf
Maxim <maxim.flyagin.1@gmail.com>
2023-02-19 15:27:28 +0300

    cards

--- client/src/screens/LoginScreen.tsx
+++ client/src/screens/LoginScreen.tsx
@@ -1,11 +1,11 @@
-import { signInWithEmailAndPassword } from "@firebase/auth";
-import React, { useEffect, useState } from "react";
-import { auth, provider } from "../../config/firebase";
-import { setUserFirestorm } from "../Redux/fireStormSlice/userFiresotrmsliceReducer";
-import { useAppDispatch, useAppSelector } from "../Redux/hooks";
-import { loginAction } from "../Redux/UserSlice/UserSliceReducer";
-import { signInWithPopup } from "@firebase/auth";
-import { Formik } from "formik";
+import { signInWithEmailAndPassword } from '@firebase/auth';
+import React, { useEffect, useState } from 'react';
+import { auth, provider } from '../../config/firebase';
+import { setUserFirestorm } from '../Redux/fireStormSlice/userFiresotrmsliceReducer';
+import { useAppDispatch, useAppSelector } from '../Redux/hooks';
+import { loginAction } from '../Redux/UserSlice/UserSliceReducer';
+import { signInWithPopup } from '@firebase/auth';
+import { Formik } from 'formik';
 import {
   Button,
   Image,
@@ -16,14 +16,14 @@ import {
   TouchableOpacity,
   TouchableWithoutFeedback,
   View,
-} from "react-native";
-import styles from "../styles/stylesall";
-import logo from "../../assets/logo.png"
+} from 'react-native';
+import styles from '../styles/stylesall';
+import logo from '../../assets/logo.png';
 
 export default function LoginScreen({ navigation }) {
   const dispatch = useAppDispatch();
   const user = useAppSelector((store) => store.user);
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
   const LoginHandler = (email, hashpass) => {
     signInWithEmailAndPassword(auth, email, hashpass)
       .then(({ user }) => {
@@ -41,10 +41,10 @@ export default function LoginScreen({ navigation }) {
   const googleHandle = () => {
     signInWithPopup(auth, provider).then((data) => {
       setValue(data.user.email);
-      localStorage.setItem("email", data.user.email);
+      localStorage.setItem('email', data.user.email);
     });
     useEffect(() => {
-      setValue(localStorage.getItem("email"));
+      setValue(localStorage.getItem('email'));
     });
   };
 
@@ -52,43 +52,57 @@ export default function LoginScreen({ navigation }) {
     <TouchableWithoutFeedback
       onPress={() => {
         Keyboard.dismiss();
-        ("dismiss keyboard");
+        ('dismiss keyboard');
       }}
     >
       <SafeAreaView style={styles.main}>
-      <Image
+        <Image
           source={logo}
           style={{
-            marginTop: 40, width: '100%', height: 160, position: 'absolute', top: 0, resizeMode: 'cover',
+            marginTop: 40,
+            width: '100%',
+            height: 160,
+            position: 'absolute',
+            top: 0,
+            resizeMode: 'cover',
           }}
         />
         <Formik
-          initialValues={{ email: "", hashpass: "" }}
+          initialValues={{ email: '', hashpass: '' }}
           onSubmit={(values, { resetForm }) => {
             LoginHandler(values.email, values.hashpass);
             console.log(values);
-            resetForm({ values: "" });
+            resetForm({ values: '' });
           }}
         >
           {(props) => (
-            <View style={{ marginTop: "60%", marginLeft: "17%" }}>
+            <View style={{ marginTop: '60%', marginLeft: '17%' }}>
               <TextInput
-              placeholder="Введите ваш логин"
+                placeholder="Введите ваш логин"
                 keyboardType="email-address"
                 style={styles.input}
-                onChangeText={props.handleChange("email")}
+                onChangeText={props.handleChange('email')}
                 value={props.values.email}
               ></TextInput>
               <TextInput
-              placeholder="Введите ваш пароль"
+                placeholder="Введите ваш пароль"
                 style={styles.input}
-                onChangeText={props.handleChange("hashpass")}
+                onChangeText={props.handleChange('hashpass')}
                 value={props.values.hashpass}
               ></TextInput>
-              <Button title="Войти" style={styles.botton} onPress={props.handleSubmit}></Button>
+              <Button
+                title="Войти"
+                style={styles.botton}
+                onPress={props.handleSubmit}
+              ></Button>
+              <Button
+                title="Gogl"
+                style={styles.botton}
+                onPress={googleHandle}
+              ></Button>
               <TouchableOpacity
-              style={styles.botton}
-                onPress={() => navigation.navigate("SignUpScreen")}
+                style={styles.botton}
+                onPress={() => navigation.navigate('SignUpScreen')}
               >
                 <Text style={styles.textBtn}>Ещё нет аккаунта?</Text>
               </TouchableOpacity>
@@ -97,6 +111,5 @@ export default function LoginScreen({ navigation }) {
         </Formik>
       </SafeAreaView>
     </TouchableWithoutFeedback>
-    
   );
 }
