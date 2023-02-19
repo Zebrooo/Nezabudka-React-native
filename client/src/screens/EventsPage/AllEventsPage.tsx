9bc328d323d6f1fadff05731dd20f5bb9611aef1
dmirtii <dimasioeremin@gmail.com>
2023-02-19 18:59:11 +0300

    end day

--- client/src/screens/EventsPage/AllEventsPage.tsx
+++ client/src/screens/EventsPage/AllEventsPage.tsx
@@ -8,13 +8,7 @@ import OneEventPage from "./OneEventPage";
 
 export default function AllEventsPage() {
   const events = useAppSelector((store) => store?.events?.events);
-  const dispatch = useAppDispatch();
 //   console.log(events);
-  useEffect(() => {
-    console.log('hello');
-    
-    dispatch(visualEventsThunk());
-  }, []);
 
   return (
     <>
