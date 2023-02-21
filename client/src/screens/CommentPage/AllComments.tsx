645c52dced5defea76ee6ea54eae61e3dc25f790
dmirtii <dimasioeremin@gmail.com>
2023-02-21 19:21:45 +0300

    somthing else

--- client/src/screens/CommentPage/AllComments.tsx
+++ client/src/screens/CommentPage/AllComments.tsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextInput } from "react-native-paper";
 import { useAppDispatch, useAppSelector } from "../../Redux/hooks";
 import CommentPage from "./CommentPage";
 
 export default function AllComments() {
+  const [input, setInput] = useState('');
   const dispatch = useAppDispatch();
   const comments = useAppSelector((state) => state.shops.oneShop.Comments);
+const SubmitHandler = () => {
+  dispatch()
+}
   return (
     <>
       {comments.map((comment) => (
         <CommentPage comment={comment} key={comment.id} />
       ))}
+      <TextInput
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      ></TextInput>
+
     </>
   );
 }
