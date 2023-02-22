4badf9b805c9ac33c7f78099ba8fec1548ab9968
dmirtii <dimasioeremin@gmail.com>
2023-02-22 10:30:54 +0300

    Push notifications

--- client/App.tsx
+++ client/App.tsx
@@ -8,16 +8,21 @@ import axios from "axios";
 import { useAppDispatch } from "./src/Redux/hooks";
 import { visualEventsThunk } from "./src/Redux/eventSlice/EventSlice";
 import CommentPage from "./src/screens/CommentPage/CommentPage";
-import NotificationScreen from "./src/screens/Notifications/NotificationScreen";
+import NotificationScreen, { schedulePushNotification } from "./src/screens/Notifications/NotificationScreen";
 import AllComments from "./src/screens/CommentPage/AllComments";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://192.168.1.56:3001";
 export default function App() {
+  const timerId = setTimeout(() => {
+    schedulePushNotification()
+  }, 2000)
+  
+  console.log(timerId)
   LogBox.ignoreAllLogs();
   return (
     <Provider store={store}>
-      <Navigate />
+      <NotificationScreen />
     </Provider>
   );
 }
